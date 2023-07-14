8bfd0c84f1e9c6b68fbe3ea5903704bfba5c49a6
Sead Xhigoli <sead.xhigoli@outlook.com>
2023-07-14 22:28:33 +0200

    home page and navbar design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // Route for the Home component
+  { path: 'dashboard', component: DashboardComponent }, // Route for the Dashboard component
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss','../shared/style.scss','../shared/bootstrap.min.css']
 })
 export class AppComponent {
   title = 'the-wellness-junction';
